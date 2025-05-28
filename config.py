c815810f55177b323f62fce29315cc1e561a7381
Alexandr Glasov <alexandr.glasov@data-ox.com>
2025-05-28 16:43:59 +0200

    Update name of spreadsheet

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 DAYS_DELTA = int(os.getenv("DAYS_DELTA"))
 
 GOOGLE_SHEET_CREDS = "data/creds.json"
-SPREADSHEET_NAME = "TelegramPostsV4"
+SPREADSHEET_NAME = "TelegramPostsV2"
 CHANNELS_FILE = "data/channels.json"
 KEYBOARD_CONFIG_FILE = "data/config.json"
