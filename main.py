9c1cf1eb57deac25307aa7ab8bf916bfe70671cc
Alexandr Glasov <alexandr.glasov@data-ox.com>
2025-05-28 17:06:14 +0200

    Add sheet initialization to entrypoint

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
 from handlers import start, post
+from utils.google_sheets import get_or_create_sheet
 
 async def main():
     bot = Bot(BOT_TOKEN)
@@ -8,6 +10,7 @@ async def main():
     dp.include_router(start.router)
     dp.include_router(post.router)
     print("BOT STARTED")
+    get_or_create_sheet()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
