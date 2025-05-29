25e14f7e744162e17a6053a1180707ea9126d670
Alexandr Glasov <alexandr.glasov@data-ox.com>
2025-05-29 12:40:17 +0200

    Update keyboards

--- keyboards/post.py
+++ keyboards/post.py
@@ -1,7 +1,9 @@
 import json
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import KEYBOARD_CONFIG_FILE
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from datetime import datetime, timedelta
+
+from config import KEYBOARD_CONFIG_FILE, DAYS_DELTA
 with open(KEYBOARD_CONFIG_FILE, "r", encoding="utf-8") as file:
     data = json.load(file)
 
@@ -12,5 +14,17 @@ def build_keyboard_from_list(items: list[str], prefix: str) -> InlineKeyboardMar
     ])
     return keyboard
 
-brand_kb = build_keyboard_from_list(data.get("brand", []), prefix="brand")
-company_kb = build_keyboard_from_list(data.get("company", []), prefix="company")
+
+class PostKeyboards:
+    brand_kb = build_keyboard_from_list(data.get("brand", []), prefix="brand")
+    company_kb = build_keyboard_from_list(data.get("company", []), prefix="company")
+
+    @property
+    @staticmethod
+    def date_kb():
+        today = datetime.now().date()
+        date_range =  [
+            (today - timedelta(days=i)).isoformat()
+            for i in range(DAYS_DELTA + 1)
+        ]
+        return build_keyboard_from_list(date_range, prefix="date")
