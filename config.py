f7d55c394dde24a15dc8a98db29b90d24b24b724
Alexandr Glasov <alexandr.glasov@data-ox.com>
2025-05-30 14:30:28 +0200

    Fix config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 DAYS_DELTA = int(os.getenv("DAYS_DELTA"))
 
 GOOGLE_SHEET_CREDS = "data/creds.json"
+ADMINS_LIST = "data/admins.json"
 SPREADSHEET_NAME = "TelegramPostsV2"
 CHANNELS_FILE = "data/channels.json"
 KEYBOARD_CONFIG_FILE = "data/config.json"
